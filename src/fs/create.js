0e31cc56b37b0bfa6818303e17875e09b8cc645c
AndreiChuyanov <achuyanov@gmail.com>
2022-12-05 17:30:19 +0300

    feat: add create method

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+// create.js - implement function that creates new file fresh.txt
+// with content I am fresh and young inside of the files folder
+// (if file already exists Error with message FS operation failed must be thrown)
+
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
+import { writeFile, access, F_OK, } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const file = join(__dirname, 'files', 'fresh.txt');
+const text = 'I am fresh and young';
+const ErrMgg = (err) => { if (err) throw err }
+
 const create = async () => {
-    // Write your code here 
+  access(file, F_OK, (err) => {
+    if (err) { writeFile(file, text, ErrMgg) }
+    else throw new Error("FS operation failed")
+  })
 };
 
 await create();
