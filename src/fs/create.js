084ea28f0b8df8041d49963a495ada2ba58a0d1c
achuyanov <achuyanov@gmail.com>
2025-10-27 00:18:48 +0300

    feat: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+
+import { writeFile, access, constants } from 'node:fs/promises';
+import { join } from 'node:path';
+
 const create = async () => {
-  // Write your code here
+
+  const
+    myFile = join("files", "fresh.txt"),
+    myErr = 'FS operation failed',
+    wrFile = () => writeFile(myFile, 'I am fresh and young', 'utf8'),
+    check = () => access(myFile, constants.F_OK);
+
+  try { await check(); throw new Error(myErr)}
+  catch (err) {
+    if (err.code === 'ENOENT') { await wrFile() }
+    else { throw new Error(myErr) }
+  }
 };
+
 
 await create();
